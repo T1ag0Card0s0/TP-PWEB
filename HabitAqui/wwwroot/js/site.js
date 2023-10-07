1b2d544896b124368012867974cfa9a061d3e72c
Tiago Cardoso <a2021138999@isec.pt>
2023-10-07 19:46:00 +0100

    add sidebar

--- HabitAqui/wwwroot/js/site.js
+++ HabitAqui/wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function toggleSidebar() {
+    const sidebarContainer = document.getElementById('mySidebar');
+    sidebarContainer.classList.toggle('open');
+
+    const openButton = document.querySelector('.openbtn');
+    openButton.classList.toggle('open');
+
+    if (openButton.classList.contains('open')) {
+        openButton.textContent = '✕ Close Sidebar';
+    } else {
+        openButton.textContent = '☰ Open Sidebar';
+    }
+}
