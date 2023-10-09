231ee754980d164eaf899b2a86e1eb850f818e89
Tiago Cardoso <a2021138999@isec.pt>
2023-10-09 13:04:38 +0100

    bootstrap styling

--- HabitAqui/wwwroot/js/site.js
+++ HabitAqui/wwwroot/js/site.js
@@ -2,16 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-function toggleSidebar() {
-    const sidebarContainer = document.getElementById('mySidebar');
-    sidebarContainer.classList.toggle('open');
-
-    const openButton = document.querySelector('.openbtn');
-    openButton.classList.toggle('open');
-
-    if (openButton.classList.contains('open')) {
-        openButton.textContent = '✕ Close Sidebar';
-    } else {
-        openButton.textContent = '☰ Open Sidebar';
-    }
-}
