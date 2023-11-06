48781ee6477b140f5222138455f88da28ad6bba8
T1ag0Card0s0 <t.rafael03.tc@gmail.com>
2023-11-06 14:56:06 +0000

    list all employees

---
+++ HabitAqui/Models/MyEmployees.cs
@@ -0,0 +1,9 @@
+﻿namespace HabitAqui.Models
+{
+    public class MyEmployees
+    {
+        public ICollection<Gestor>? Gestores { get; set; }
+        public ICollection<Funcionario>? Funcionarios { get; set; }
+
+    }
+}
