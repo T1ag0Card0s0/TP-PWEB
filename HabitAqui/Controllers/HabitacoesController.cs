ba29a2afc93f7b52969c01d6a03937478934ae26
Daniela Correia <danielafernandescorreia@gmail.com>
2023-11-07 12:16:04 +0000

    add details

--- HabitAqui/Controllers/HabitacoesController.cs
+++ HabitAqui/Controllers/HabitacoesController.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace HabitAqui.Controllers
 {
+    
     [Authorize(Roles = "Admin,Cliente,Gestor, Funcionario")]
     public class HabitacoesController : Controller
     {
