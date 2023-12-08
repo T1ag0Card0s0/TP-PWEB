c996a945435d300481939489e3b0e45a195dfdf8
danifc10 <danielafernandescorreia@gmail.com>
2023-12-08 17:05:39 +0000

    bug resolved

---
+++ HabitAqui/Migrations/20231208170229_migration26.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HabitAqui.Migrations
+{
+    public partial class migration26 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Imagens_Arrendamentos_ArrendamentoId",
+                table: "Imagens");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ArrendamentoId",
+                table: "Imagens",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Imagens_Arrendamentos_ArrendamentoId",
+                table: "Imagens",
+                column: "ArrendamentoId",
+                principalTable: "Arrendamentos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Imagens_Arrendamentos_ArrendamentoId",
+                table: "Imagens");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ArrendamentoId",
+                table: "Imagens",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Imagens_Arrendamentos_ArrendamentoId",
+                table: "Imagens",
+                column: "ArrendamentoId",
+                principalTable: "Arrendamentos",
+                principalColumn: "Id");
+        }
+    }
+}
--- HabitAqui/Models/Imagem.cs
+++ HabitAqui/Models/Imagem.cs
@@ -1,8 +1,15 @@
-﻿namespace HabitAqui.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HabitAqui.Models
 {
     public class Imagem
     {
         public int Id { get; set; }
         public string Path { get; set; }
+
+        [ForeignKey("Arrendamento")]
+        public int ArrendamentoId { get; set; }
+        public Arrendamento Arrendamento { get; set; }
     }
+
 }
