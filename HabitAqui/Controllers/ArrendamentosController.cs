4ca98cf80e461b4867e263ce2990486b6f344b0d
T1ag0Card0s0 <t.rafael03.tc@gmail.com>
2023-12-11 16:35:25 +0000

    add radar chart

--- HabitAqui/Controllers/ArrendamentosController.cs
+++ HabitAqui/Controllers/ArrendamentosController.cs
@@ -549,6 +549,8 @@ namespace HabitAqui.Controllers
             var arrendamento = await _context.Arrendamentos
                 .Include(a=>a.EntregarArrendamento)
                 .Include(a=>a.ReceberArrendamento)
+                .Include(a=> a.EntregarArrendamento.Equipamentos)
+                .Include(a=> a.ReceberArrendamento.Equipamentos)
                 .FirstOrDefaultAsync(a => a.Id == id);
 
             if (arrendamento != null)
